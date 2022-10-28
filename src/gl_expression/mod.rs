059a585d2291526572f5b704dc7ee3e4dc53766c
Edward Taylor <edeetee@gmail.com>
2022-10-29 12:41:20 +1300

    glsl expr auto wrapping

--- src/gl_expression/mod.rs
+++ src/gl_expression/mod.rs
@@ -1,4 +1,5 @@
 use glium::{backend::Facade, Surface, uniforms::Uniforms, program::UniformBlock};
+use glsl::{syntax::{Expr, ShaderStage, Identifier}, parser::Parse};
 
 use crate::fullscreen_shader::FullscreenFrag;
 
@@ -23,7 +24,7 @@ impl GlExpressionRenderer {
                 Some(uniform_data)
             },
             Err(err) => {
-                eprintln!("Error {err} compiling {shader}\n");
+                eprintln!("Compiling '{shader}': Error {err}\nwhen ");
                 None
             }
         }
@@ -41,16 +42,84 @@ impl GlExpressionRenderer {
     }
 }
 
+///Always defaults to Vec4
+#[derive(PartialEq, PartialOrd, Eq, Ord)]
+enum GlType {
+    Float,
+    Vec2,
+    Vec3,
+    Vec4
+}
+
+impl GlType {
+    fn from_length(len: usize) -> GlType {
+        match len {
+            1 => Self::Float,
+            2 => Self::Vec2,
+            3 => Self::Vec3,
+            _ => Self::Vec4
+        }
+    }
+
+    // fn from_iden(iden: &str) -> GlType {
+    //     match iden {
+    //         "pixel" => Self::Vec4,
+
+    //     }
+    // }
+}
+
+fn parse_gl_type(expr: &Expr) -> GlType {
+    match expr {
+        Expr::FloatConst(_) => GlType::Float,
+        Expr::Dot(_, Identifier(iden)) => GlType::from_length(iden.len()),
+        Expr::FunCall(glsl::syntax::FunIdentifier::Identifier(Identifier(fun_ident)), _) => {
+            match fun_ident.as_str() {
+                "vec2" => GlType::Vec2,
+                "vec3" => GlType::Vec3,
+                _ => GlType::Vec4
+            }
+        }
+        Expr::Binary(_, a, b) => parse_gl_type(a).max(parse_gl_type(b)),
+        _ => GlType::Vec4
+    }
+}
+
 fn build_shader_from_snippet(snippet: &str) -> String {
-    format!("
+    let expression = Expr::parse(snippet);
+    dbg!(&expression);
+
+    let snippet = snippet.to_string();
+
+    let wrapped_snippet = match &expression {
+        Ok(expr) => {
+            match parse_gl_type(expr) {
+                GlType::Float => format!("vec4(vec3({snippet}),1)"),
+                GlType::Vec2 => format!("vec4({snippet}, 0,1)"),
+                GlType::Vec3 => format!("vec4({snippet},1"),
+                GlType::Vec4 => snippet
+            }
+        },
+        Err(err) => {
+            eprintln!("{}", err);
+            snippet
+        }
+    };
+
+    let shader_str = format!("
     #version 140
     uniform sampler2D pixels;
     
     void main() {{
         vec4 pixel = texture2D(pixels, gl_FragCoord.xy/textureSize(pixels, 0));
-        gl_FragColor = {snippet};
+        gl_FragColor = {wrapped_snippet};
     }}
-    ")
+    ");
+
+    // let stage = ShaderStage::parse(&shader_str);
+    // dbg!(stage);
+
+    shader_str
 }
 
 pub struct GlExpressionUpdater {
