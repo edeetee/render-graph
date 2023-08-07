9d37f82e59ec971e8a97acbd656b82e2b0b6eb7d
Edward Taylor <edeetee@gmail.com>
2023-08-07 13:17:03 +1200

    hmmm

--- src/bevy_main.rs
+++ src/bevy_main.rs
@@ -3,21 +3,16 @@ use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
 use crate::{editor::graph_ui::GraphUi, common::persistent_state::PersistentState};
 
-
 pub fn main() {
 
     let state = PersistentState::load_from_default_path();
     let mut graph_ui = GraphUi::new_from_persistent(state, &display, &mut egui_glium);
 
     App::new()
-        .add_plugins(DefaultPlugins)
+        // .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
         // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
         // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
         .add_systems(Update, )
         .run();
-}
-
-fn setup_graph_ui(mut contexts: EguiContexts, mut graph_ui: ResMut<GraphUi>) {
-    
 }
