b7d99312406b38321b0cb24a2b6b8c2f0d539574
Edward Taylor <edeetee@gmail.com>
2023-09-25 14:31:06 +1300

    very close, just have to fix blit mapping

--- src/ffgl_plugin.rs
+++ src/ffgl_plugin.rs
@@ -1,23 +1,28 @@
-use std::{fmt::Formatter, rc::Rc, time::SystemTime};
+use std::{borrow::BorrowMut, fmt::Formatter, rc::Rc, sync::Once, time::SystemTime};
 
-use ffgl::logln;
+use ffgl::{logln, validate};
 // use egui_node_graph::graph;
 // mod ffgl;
 use ::ffgl::{ffgl_handler, FFGLHandler};
-use gl::types;
+use gl::{types, FRAMEBUFFER};
 use glium::{
     backend::{Context, Facade},
-    framebuffer::{ColorAttachment, EmptyFrameBuffer, SimpleFrameBuffer},
+    framebuffer::{ColorAttachment, EmptyFrameBuffer, RenderBuffer, SimpleFrameBuffer},
     texture::{TextureAny, TextureAnyImage},
-    BlitMask, CapabilitiesSource, Frame, GlObject, Rect, Surface, Texture2d,
+    BlitMask, BlitTarget, CapabilitiesSource, Display, Frame, GlObject, Rect, Surface, Texture2d,
 };
 use naga::back;
 
 use crate::{
     common::persistent_state::PersistentState,
-    graph::{def::GraphEditorState, ShaderGraphProcessor},
+    graph::{
+        def::{GraphEditorState, GraphState},
+        ShaderGraphProcessor,
+    },
     textures,
 };
+
+const GL_INIT_ONCE: Once = std::sync::Once::new();
 
 struct RawGlBackend {
     size: (u32, u32),
@@ -26,8 +31,10 @@ struct RawGlBackend {
 impl RawGlBackend {
     ///Only run once!!!
     fn new(size: (u32, u32)) -> Self {
-        gl_loader::init_gl();
-        gl::load_with(|s| gl_loader::get_proc_address(s).cast());
+        GL_INIT_ONCE.call_once(|| {
+            gl_loader::init_gl();
+            gl::load_with(|s| gl_loader::get_proc_address(s).cast());
+        });
 
         Self { size }
     }
@@ -57,8 +64,10 @@ unsafe impl glium::backend::Backend for RawGlBackend {
 
 pub struct RenderGraphHandler {
     graph: crate::graph::def::Graph,
+    graph_state: GraphState,
     processor: crate::graph::ShaderGraphProcessor,
     texture_manager: crate::textures::TextureManager,
+    backend: Rc<RawGlBackend>,
     ctx: Rc<Context>,
 }
 
@@ -73,18 +82,18 @@ impl std::fmt::Debug for RenderGraphHandler {
 
 impl FFGLHandler for RenderGraphHandler {
     unsafe fn new(inst_data: &ffgl::FFGLData) -> Self {
-        let backend = RawGlBackend::new(inst_data.get_dimensions());
-
-        logln!("path: {:?}", PersistentState::default_path());
+        let backend = Rc::new(RawGlBackend::new(inst_data.get_dimensions()));
 
         let state = PersistentState::load_from_default_path();
 
         let mut graph = state.editor.graph;
 
-        logln!("graph {:#?}", graph);
+        for (node_id, node) in &graph.nodes {
+            logln!("{node_id:?}: {node:#?}");
+        }
 
         let ctx = glium::backend::Context::new(
-            backend,
+            backend.clone(),
             false,
             glium::debug::DebugCallbackBehavior::Custom {
                 callback: Box::new(|src, typ, sev, a, b, c| {
@@ -103,7 +112,9 @@ impl FFGLHandler for RenderGraphHandler {
         logln!("OPENGL_VERSION {}", ctx.get_opengl_version_string());
 
         Self {
+            backend,
             processor: ShaderGraphProcessor::new_from_graph(&mut graph, &ctx),
+            graph_state: state.state,
             graph,
             texture_manager,
             ctx,
@@ -111,35 +122,157 @@ impl FFGLHandler for RenderGraphHandler {
     }
 
     unsafe fn draw(&mut self, inst_data: &ffgl::FFGLData, frame_data: &ffgl::ProcessOpenGLStruct) {
-        let (width, height) = inst_data.get_dimensions();
-        let mod_sec = inst_data
-            .host_time
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs_f32()
-            % 1.0;
+        //reset to what glium expects the state to be in
+        gl::BindFramebuffer(FRAMEBUFFER, 0);
+        self.ctx.rebuild(self.backend.clone()).unwrap();
 
-        let mut frame = Frame::new(self.ctx.clone(), inst_data.get_dimensions());
-        frame.clear_color(0.0, 1.0 - mod_sec, 0.0, 1.0);
+        let res = inst_data.get_dimensions();
+
+        //Higher resolution than this seems to cut off the image
+        let mut frame = Frame::new(self.ctx.clone(), (128, 128));
+        let frame_dimen = frame.get_dimensions();
+
+        validate::validate_context_state();
+        self.render_frame(inst_data, &mut frame);
+
+        frame.finish().unwrap();
+        self.ctx.finish();
+
+        gl::BindFramebuffer(gl::READ_FRAMEBUFFER, 0);
+        gl::BindFramebuffer(gl::DRAW_FRAMEBUFFER, frame_data.HostFBO);
+        blit_fb(frame_dimen, res);
+
+        //reset to what host expects
+        gl_reset(frame_data);
+    }
+}
+
+impl RenderGraphHandler {
+    /**
+    ### WORKING:
+     - Texture/Frame Clear
+     - Texture -> Frame
+     - Frame -> Output
+
+     ### Not working:
+     - shader render texture seems to be empty
+    */
+    fn render_frame(&mut self, inst_data: &ffgl::FFGLData, frame: &mut Frame) {
+        let ramp = 1.0
+            - inst_data
+                .host_time
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs_f32()
+                % 1.0;
+
+        frame.clear_color(ramp, 0.0, 0.0, 1.0);
+
+        self.processor
+            .update(&mut self.graph, &self.graph_state, &self.ctx);
 
         self.processor.render_shaders(
             &mut self.graph,
             &self.ctx,
             &mut self.texture_manager,
             |node_id, tex| {
+                // gl::UseProgram(0);
+                // gl::ActiveTexture(gl::TEXTURE0);
+                // validate::validate_context_state();
                 //both required for the texture to be available...
                 //TODO: WHY?
 
+                // tex.as_surface()
+                //     .fill(&frame, glium::uniforms::MagnifySamplerFilter::Nearest);
+                // tex.as_surface().clear_color(0.0, ramp, 0.0, 1.0);
+
+                // frame.clear_color(ramp, 0.0, ramp, 1.0);
+
                 tex.as_surface()
-                    .fill(&frame, glium::uniforms::MagnifySamplerFilter::Nearest);
+                    .fill(frame, glium::uniforms::MagnifySamplerFilter::Nearest);
+
+                // let src = tex.as_surface();
+                // let target = &frame;
+
+                // let src_dim = src.get_dimensions();
+                // let src_rect = Rect {
+                //     left: 0,
+                //     bottom: 0,
+                //     width: src_dim.0 as u32,
+                //     height: src_dim.1 as u32,
+                // };
+                // let target_dim = target.get_dimensions();
+                // let target_rect = glium::BlitTarget {
+                //     left: 0,
+                //     bottom: 0,
+                //     width: target_dim.0 as i32,
+                //     height: target_dim.1 as i32,
+                // };
+
+                // target.blit_buffers_from_simple_framebuffer(
+                //     &src,
+                //     &src_rect,
+                //     &target_rect,
+                //     glium::uniforms::MagnifySamplerFilter::Nearest,
+                //     BlitMask::color(),
+                // );
+
+                // copy(tex.get_id(), res, 0, res);
                 // copy_hostfbo(&tex.as_surface(), &frame);
-                copy(tex, frame_data.HostFBO, width, height);
+                // copy(tex, frame_data.HostFBO, width, height);
             },
         );
-
-        frame.finish().unwrap();
-        gl::BindFramebuffer(gl::FRAMEBUFFER, frame_data.HostFBO);
+        for (node_id, node) in self.graph.nodes.iter() {
+            for err in vec![
+                &node.user_data.render_error,
+                &node.user_data.create_error,
+                &node.user_data.update_error,
+            ] {
+                if let Some(err) = err {
+                    logln!("ERROR on {node_id:?}: {err:?}");
+                }
+            }
+        }
     }
+}
+
+struct TextureType {
+    target: u32,
+    binding: u32,
+}
+const TEXTURE_TYPES: [TextureType; 2] = [
+    TextureType {
+        target: gl::TEXTURE_1D,
+        binding: gl::TEXTURE_BINDING_1D,
+    },
+    TextureType {
+        target: gl::TEXTURE_2D,
+        binding: gl::TEXTURE_BINDING_2D,
+    },
+    // Add other texture types here...
+];
+
+unsafe fn gl_reset(frame_data: &ffgl::ProcessOpenGLStructTag) {
+    let mut gl_int = 0;
+    gl::UseProgram(0);
+
+    let mut num_samplers = 0;
+    gl::GetIntegerv(gl::MAX_TEXTURE_IMAGE_UNITS, &mut num_samplers);
+
+    for texture_type in TEXTURE_TYPES.iter() {
+        for sampler in 0..num_samplers {
+            gl::ActiveTexture(gl::TEXTURE0 + sampler as u32);
+            // Check if textures are unbound for the current texture unit.
+            gl::GetIntegerv(texture_type.binding, &mut gl_int);
+            gl::BindTexture(texture_type.target, 0);
+        }
+    }
+
+    gl::ActiveTexture(gl::TEXTURE0);
+
+    // gl::VertexArrayElementBuffer(vaobj, buffer)
+    // gl::BindTextureUnit(0, 0);
+    gl::BindFramebuffer(gl::FRAMEBUFFER, frame_data.HostFBO);
 }
 
 // fn copy_hostfbo(src: &impl Surface, target: &impl Surface) {
@@ -165,37 +298,32 @@ impl FFGLHandler for RenderGraphHandler {
 //     )
 // }
 
-unsafe fn copy(tex: &Texture2d, frame_data: gl::types::GLuint, width: u32, height: u32) {
-    gl::BindFramebuffer(gl::READ_FRAMEBUFFER, tex.get_id());
-    gl::BindFramebuffer(gl::DRAW_FRAMEBUFFER, frame_data);
-
-    let (tex_w, tex_h) = tex.dimensions();
-
-    let src_rect = Rect {
+unsafe fn blit_fb((read_w, read_h): (u32, u32), (write_w, write_h): (u32, u32)) {
+    let src_rect = BlitTarget {
         left: 0,
         bottom: 0,
-        width: tex_w / 2,
-        height: tex_h,
+        width: (read_w as i32),
+        height: (read_h as i32),
     };
 
-    let target_rect = Rect {
-        left: 0,
-        bottom: 0,
-        width: width / 2,
-        height,
+    let target_rect = BlitTarget {
+        left: (write_w as f32 * 0.25) as u32,
+        bottom: (write_h as f32 * 0.25) as u32,
+        width: (write_w as i32) / 2,
+        height: (write_h as i32) / 2,
     };
 
     gl::BlitFramebuffer(
         src_rect.left as gl::types::GLint,
         src_rect.bottom as gl::types::GLint,
-        (src_rect.left + src_rect.width) as gl::types::GLint,
-        (src_rect.bottom + src_rect.height) as gl::types::GLint,
-        target_rect.left as gl::types::GLint,
-        target_rect.bottom as gl::types::GLint,
-        (target_rect.left as i32 + target_rect.width as i32) as gl::types::GLint,
-        (target_rect.bottom as i32 + target_rect.height as i32) as gl::types::GLint,
+        (src_rect.left as i32 + src_rect.width) as gl::types::GLint,
+        (src_rect.bottom as i32 + src_rect.height) as gl::types::GLint,
+        (target_rect.left) as gl::types::GLint,
+        (target_rect.bottom) as gl::types::GLint,
+        (target_rect.left as i32 + target_rect.width) as gl::types::GLint,
+        (target_rect.bottom as i32 + target_rect.height) as gl::types::GLint,
         gl::COLOR_BUFFER_BIT,
-        gl::LINEAR,
+        gl::NEAREST,
     );
 }
 
